546a8332ad4221ba4fca548ec907144bf9c887cf
Bharath <gunamonibharath@gmail.com>
2024-10-21 17:03:50 +0530

    initial commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,172 @@
+import "regenerator-runtime/runtime";
+
+import React, { useContext, useEffect, useRef, useState } from "react";
+import "./main.css";
+import { assets } from "../../assets/assets/assets";
+import { Context } from "../../context/Context";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+function Main() {
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
+
+  const { transcript, listening, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  const [phase1, setPhase1] = useState(
+    "Suggest beautiful places to see on an upcoming road trip"
+  );
+  const [phase2, setPhase2] = useState(
+    "Breifly sumarize this concept: urban planning"
+  );
+  const [phase3, setPhase3] = useState(
+    "Discuss the concept of 'justice' according to Plato's Republic"
+  );
+  const [phase4, setPhase4] = useState(
+    "Improve the readability of the following code"
+  );
+  const resultRef = useRef(null);
+  useEffect(() => {
+    scrollToBottom();
+  }, [resultData]);
+  const scrollToBottom = () => {
+    if (resultRef.current) {
+      resultRef.current.scrollTop = resultRef.current.scrollHeight;
+    }
+  };
+  const handleChange = (e) => {
+    setInput(e.target.value);
+    const numberOfLines = e.target.value.split("\n").length;
+    setInput(e.target.value);
+    if (numberOfLines === 0) {
+      e.target.rows = 1;
+    } else if (numberOfLines <= 5) {
+      e.target.rows = numberOfLines;
+    } else {
+      e.target.rows = 5;
+    }
+  };
+
+  const speakNow = () => {
+    if (!browserSupportsSpeechRecognition) {
+      alert("Your Browser doesn't support speech recognition.");
+      return;
+    }
+
+    SpeechRecognition.startListening();
+  };
+
+  useEffect(()=>{
+    setInput(transcript)
+  }, [transcript])
+  return (
+    <>
+      <div className="main">
+        <div className="nav">
+          <p>
+            Gemini Clone <img src={assets.gemini_icon} alt="" />
+          </p>
+          <img src={assets.user_icon} alt="" />
+        </div>
+        <div className="main-container">
+          {!showResult ? (
+            <>
+              <div className="greet">
+                <p>
+                  <span>Hello, Dev.</span>
+                </p>
+                <p>How can i help you today?</p>
+              </div>
+              <div className="cards">
+                <div className="card" onClick={() => setInput(phase1)}>
+                  <p>{phase1}</p>
+                  <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card" onClick={() => setInput(phase2)}>
+                  <p>{phase2}</p>
+                  <img src={assets.bulb_icon} alt="" />
+                </div>
+                <div className="card" onClick={() => setInput(phase3)}>
+                  <p>{phase3}</p>
+                  <img src={assets.message_icon} alt="" />
+                </div>
+                <div className="card" onClick={() => setInput(phase4)}>
+                  <p>{phase4}</p>
+                  <img src={assets.code_icon} alt="" />
+                </div>
+              </div>
+            </>
+          ) : (
+            <div className="result" ref={resultRef}>
+              <div className="result-title">
+                <img src={assets.user_icon} alt="" />
+                <p>{recentPrompt}</p>
+              </div>
+              <div className="result-data">
+                <img src={assets.gemini_icon} alt="" />
+                {loading ? (
+                  <div className="loader">
+                    <hr />
+                    <hr />
+                    <hr />
+                  </div>
+                ) : (
+                  <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                )}
+              </div>
+            </div>
+          )}
+
+          <div className="main-bottom">
+            <div className="search-box">
+              <textarea
+                onChange={handleChange}
+                value={input}
+                type="text"
+                rows={1}
+                placeholder="Enter your prompt here..."
+              />
+              <div>
+                <img
+                 title="Upload Image"
+                  src={assets.gallery_icon}
+                  alt=""
+                  onClick={() => alert("Buy me a coffee to use this feature.")}
+                />
+                {listening ? "On" : null}
+                <img
+                 title="Voice search"
+                  src={assets.mic_icon}
+                  alt=""
+                  onClick={speakNow}
+                />
+
+                {input ? (
+                  <img  title="Send" onClick={() => onSent()} src={assets.send_icon} alt="" />
+                ) : null}
+              </div>
+            </div>
+            <p className="bottom-info">
+              Gemini may display inaccurate information, including about people,
+              so double check its responses.{" "}
+              <a target="_blank" href="https://sourav-c-portfolio.netlify.app">
+                Developed by Sourav.
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Main;
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,68 @@
+import React, { useContext, useState } from "react";
+import "./sidebar.css";
+import { assets } from "../../assets/assets/assets";
+import { Context } from "../../context/Context";
+function Sidebar() {
+  const [extended, setExtended] = useState(false);
+  const {onSent, prevPrompts, setRecentPrompt, newChat} = useContext(Context);
+
+  const loadPrompt = async (prompt) =>{
+    setRecentPrompt(prompt)
+    await onSent(prompt)
+  }
+  return (
+    <>
+      <div className="sidebar">
+        <div className="top">
+        {extended ? <img
+            onClick={() => setExtended((prev) => !prev)}
+            className="menu"
+            src={assets.cross_icon}
+            alt=""
+            srcset=""
+          /> :  <img
+            onClick={() => setExtended((prev) => !prev)}
+            className="menu"
+            src={assets.menu_icon}
+            alt=""
+            srcset=""
+          />}
+          
+          <div onClick={()=>newChat()} className="new-chat">
+            <img src={assets.plus_icon} alt="" />
+            {extended ? <p>New Chat</p> : null}
+          </div>
+          {extended ? (
+            <div className="recent">
+              <p className="recent-title">Recent</p>
+              {prevPrompts.map((item, index) => {
+                return (
+                  <div onClick={()=>loadPrompt(item)} className="recent-entry">
+                    <img src={assets.message_icon} alt=""  />
+                    <p>{item.slice(0,20)}...</p>
+                  </div>
+                );
+              })}
+            </div>
+          ) : null}
+        </div>
+        <div className="bottom">
+          <div className="bottom-item recent-entry">
+            <img src={assets.question_icon} alt="" title="Help" />
+            {extended ? <p>Help</p> : null}
+          </div>
+          <div className="bottom-item recent-entry">
+            <img src={assets.history_icon} alt=""  title="History"/>
+            {extended ? <p>Activity</p> : null}
+          </div>
+          <div className="bottom-item recent-entry">
+            <img src={assets.setting_icon} alt=""  title="Setting"/>
+            {extended ? <p>Setting</p> : null}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Sidebar;
